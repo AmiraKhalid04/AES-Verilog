a05329d665acd5537c9b032ea2ecbb69dc771d26
amirakhaled928 <amirakhaled928@gmail.com>
2024-05-01 21:18:08 +0300

    Adding sub-bytes /inv-sub-bytes/add-round-key +their test benches.

--- Algo.v
+++ Algo.v
@@ -5,9 +5,15 @@ endmodule
 //You may need to modifing ports of the functions you use here
 //Just Add the ports only
 
-module InvCipher();
+module InvCipher(
+    input [127:0]in ,
+    input [127:0]key ,
+    output[127:0]out ); //in is Cipher --out is PlainText
 //TODO C: Design this Module
+   
+
 endmodule
+
 
 //You may need to modifing ports of the functions you use here
 //Just Add the ports only
