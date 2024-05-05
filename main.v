1fe92d7dd7f8ba8d953c1a52a6aa6bc5519f2083
amirakhaled928 <amirakhaled928@gmail.com>
2024-05-05 10:00:45 +0300

    Revert "bonus"
    This reverts commit 7456fb94ea7f0ecd1e5b94025a353487cdaffdaf.

--- main.v
+++ main.v
@@ -1,32 +1,18 @@
 //TODO A_1 :implement Binary -> BCD Converter
 module BCD_Converter();
 endmodule
+
 //TODO A_2 :implement BCD -> 7-segment Decoder
 module Seg7_Decoder();
 endmodule
 
-module main(input [1:0]SW,out);
+module main(out);
     reg input_text;
     output out;
-    always @(*)begin
-    if(SW==0)
     begin
         KeyExpansion KEx();
         Cipher C();
         invCipher iC();
-    end
-    else if(SW==1)
-    begin
-        KeyExpansion_192 KEx();
-        Cipher_192 C();
-        invCipher_192 iC();
-    end    
-     else if(SW==2)
-    begin
-        KeyExpansion_256 KEx();
-        Cipher_256 C();
-        invCipher_256 iC();
-    end    
     end
     //After modifing input_text and out don't forget to
     //Set the leastsig to the previous mods and this is your output
