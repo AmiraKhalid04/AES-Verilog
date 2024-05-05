ff0d014196cd29c07fd7dd0ec90b0cf11b5a120d
amirakhaled928 <amirakhaled928@gmail.com>
2024-05-05 09:59:31 +0300

    Revert "bonus"
    This reverts commit 0d6b12b3b3aca16c07b6bd937c369d034cc03cab.

--- add_round_key_256.v
+++
@@ -1,7 +0,0 @@
-
-module add_round_key_256(state,key,next_state);
-    input [0:255] key;
-    input [0:127] state;
-    output [0:127] next_state;
-    assign next_state =key^state;
-endmodule
--- main.v
+++ main.v
@@ -6,11 +6,10 @@ module Seg7_Decoder();
 endmodule
 
 module main(input [1:0]SW,out);
-    reg input_text_128,input_text_192_input_text_256;
-    assign input_text_128='h
+    reg input_text;
     output out;
     always @(*)begin
-    if(SW==0||SW==3)
+    if(SW==0)
     begin
         KeyExpansion KEx();
         Cipher C();
@@ -18,15 +17,15 @@ module main(input [1:0]SW,out);
     end
     else if(SW==1)
     begin
-        KeyExpansion_192 KEx_1();
-        Cipher_192 C_1();
-        invCipher_192 iC_1();
+        KeyExpansion_192 KEx();
+        Cipher_192 C();
+        invCipher_192 iC();
     end    
      else if(SW==2)
     begin
-        KeyExpansion_256 KEx_2();
-        Cipher_256 C_2();
-        invCipher_256 iC_2();
+        KeyExpansion_256 KEx();
+        Cipher_256 C();
+        invCipher_256 iC();
     end    
     end
     //After modifing input_text and out don't forget to
